9d0a2c0200f865238e796fada4fdc53376fdfd13
설송우 <pl9429@naver.com>
2023-10-05 18:22:04 +0900

    Initial commit

---
+++ src/main/java/hello/core/KimyunghanSpring1Application.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimyunghanSpring1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimyunghanSpring1Application.class, args);
+    }
+
+}
